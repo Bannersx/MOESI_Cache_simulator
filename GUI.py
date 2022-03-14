d01560af8f46a5a03f62de1084e90d5df6c4a8e2
Bannersx <38147810+Bannersx@users.noreply.github.com>
2022-03-14 01:03:01 -0600

    Fixed Processor threads
    Processor threads now correctly update the UI. Memory updates are not yet implemented

--- GUI.py
+++ GUI.py
@@ -1,5 +1,5 @@
-import tkthread; tkthread.tkinstall()
-from concurrent.futures import process
+from turtle import width
+import tkthread;
 import threading
 import tkinter as tk
 from tkinter import *
@@ -12,13 +12,13 @@ from memory import *
 #F5F5F2
 class SampleApp(tk.Tk):
 
-    def __init__(self,processors, *args, **kwargs):
+    def __init__(self,processors,memory, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self.title_font = tkfont.Font(family='Helvetica', size=22, weight="bold", slant="italic")
         self.title("MOESI CACHE SIMULATOR")
-        self.geometry("1550x950")
+        self.geometry("1550x1000")
 
-        tkt = tkthread.TkThread(self)
+        #tkt = tkthread.TkThread(self)
         # the container is where we'll stack a bunch of frames
         # on top of each other, then the one we want visible
         # will be raised above the others
@@ -30,7 +30,7 @@ class SampleApp(tk.Tk):
         self.frames = {}
         for F in (StartPage, PageOne, PageTwo):
             page_name = F.__name__
-            frame = F(parent=container, controller=self,processors=processors)
+            frame = F(parent=container, controller=self,processors=processors,memory=memory)
             self.frames[page_name] = frame
 
             # put all of the pages in the same location;
@@ -49,7 +49,7 @@ class SampleApp(tk.Tk):
 
 class StartPage(tk.Frame):
 
-    def __init__(self, parent, controller,processors):
+    def __init__(self, parent, controller,processors,memory):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         label = tk.Label(self, text="This is the start page", font=controller.title_font)
@@ -57,20 +57,30 @@ class StartPage(tk.Frame):
 
         button1 = tk.Button(self, text="Step by Step",
                             command=lambda: controller.show_frame("PageOne"))
-        button2 = tk.Button(self, text="continuous",
+        button2 = tk.Button(self, text="Continuous",
                             command=lambda: controller.show_frame("PageTwo"))
+        button3 = tk.Button(self, text="Exit",
+                            command=self.exitFunction)
         button1.pack(pady=20)
         button2.pack()
-        
+        button3.pack()
 
+    def exitFunction(self):
+        tk.Tk.quit(self)
 
 class PageOne(tk.Frame):
 
-    def __init__(self, parent, controller,processors):
+    def __init__(self, parent, controller,processors,memory):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+        self.processors = processors
+        self.CACHE1_L1_1 = ""
+        self.CACHE1_L1_2 = ""
+        self.CACHE1_L1_3 = ""
+        self.CACHE1_L1_4 = ""
+        self.memory = memory
         label = tk.Label(self, text="Step by Step execution", font=controller.title_font)
-        label.place(x=650,y=20)
+        label.place(x=670,y=20)
         
         # Constructing the elements.
         # Entire Rectangle of the CPU
@@ -85,14 +95,14 @@ class PageOne(tk.Frame):
         CONTROLLER3 = tk.Text(self, height = 7, width = 35)
         CONTROLLER4 = tk.Text(self, height = 7, width = 35)
         
-        CPU1.place(x=30,y=100)
-        CPU2.place(x=410,y=100)
-        CPU3.place(x=790,y=100)
-        CPU4.place(x=1170,y=100)
-        CONTROLLER1.place(x=65,y=400)
-        CONTROLLER2.place(x=445,y=400)
-        CONTROLLER3.place(x=825,y=400)
-        CONTROLLER4.place(x=1205,y=400)
+        CPU1.place(x=30,y=65)
+        CPU2.place(x=410,y=65)
+        CPU3.place(x=790,y=65)
+        CPU4.place(x=1170,y=65)
+        CONTROLLER1.place(x=65,y=360)
+        CONTROLLER2.place(x=445,y=360)
+        CONTROLLER3.place(x=825,y=360)
+        CONTROLLER4.place(x=1205,y=360)
 
         # CPU ID Label
         CPU1.create_text(175,20, fill="White", text="CPU 1",font=tkfont.Font(family='Times', size=16, weight="bold"))
@@ -107,25 +117,25 @@ class PageOne(tk.Frame):
         CPU4.create_text(175,270, fill="White", text="CONTROLLER",font=tkfont.Font(family='Times', size=16, weight="bold"))
 
         # Cache for CPU1
-        CACHE1_L1_1 = tk.Text(self, height = 4, width = 10, font=tkfont.Font(family='Helvetica', size=14, weight="bold"),bg="#bde3ff")
-        for key in processors[0].CACHE['B0']:
-            CACHE1_L1_1.insert(END, processors[0].CACHE['B0'][key]+"\n")
-        CACHE1_L1_1.place(x=92,y=140)
+        self.CACHE1_L1_1 = tk.Text(self, height = 4, width = 10, font=tkfont.Font(family='Helvetica', size=14, weight="bold"),bg="#bde3ff")
+        for key in self.processors[0].CACHE['B0']:
+            self.CACHE1_L1_1.insert(END, self.processors[0].CACHE['B0'][key]+"\n")
+        self.CACHE1_L1_1.place(x=92,y=110)
 
-        CACHE1_L1_2 = tk.Text(self, height = 4, width = 10, font=tkfont.Font(family='Helvetica', size=14, weight="bold"),bg="#bde3ff")
-        for key in processors[0].CACHE['B1']:
-            CACHE1_L1_2.insert(END, processors[0].CACHE['B1'][key]+"\n")
-        CACHE1_L1_2.place(x=209,y=140)
+        self.CACHE1_L1_2 = tk.Text(self, height = 4, width = 10, font=tkfont.Font(family='Helvetica', size=14, weight="bold"),bg="#bde3ff")
+        for key in self.processors[0].CACHE['B1']:
+            self.CACHE1_L1_2.insert(END, self.processors[0].CACHE['B1'][key]+"\n")
+        self.CACHE1_L1_2.place(x=209,y=110)
 
-        CACHE1_L1_3 = tk.Text(self, height = 4, width = 10, font=tkfont.Font(family='Helvetica', size=14, weight="bold"),bg="#bde3ff")
-        for key in processors[0].CACHE['B2']:
-            CACHE1_L1_3.insert(END, processors[0].CACHE['B2'][key]+"\n")
-        CACHE1_L1_3.place(x=92,y=235)
+        self.CACHE1_L1_3 = tk.Text(self, height = 4, width = 10, font=tkfont.Font(family='Helvetica', size=14, weight="bold"),bg="#bde3ff")
+        for key in self.processors[0].CACHE['B2']:
+            self.CACHE1_L1_3.insert(END, self.processors[0].CACHE['B2'][key]+"\n")
+        self.CACHE1_L1_3.place(x=92,y=205)
 
-        CACHE1_L1_4 = tk.Text(self, height = 4, width = 10, font=tkfont.Font(family='Helvetica', size=14, weight="bold"),bg="#bde3ff")
-        for key in processors[0].CACHE['B3']:
-            CACHE1_L1_4.insert(END, processors[0].CACHE['B3'][key]+"\n")
-        CACHE1_L1_4.place(x=209,y=235)
+        self.CACHE1_L1_4 = tk.Text(self, height = 4, width = 10, font=tkfont.Font(family='Helvetica', size=14, weight="bold"),bg="#bde3ff")
+        for key in self.processors[0].CACHE['B3']:
+            self.CACHE1_L1_4.insert(END, self.processors[0].CACHE['B3'][key]+"\n")
+        self.CACHE1_L1_4.place(x=209,y=205)
         # Cache for CPU2
         CPU2.create_rectangle(40,50,175,150,fill="#bde3ff")
         CPU2.create_rectangle(175,50,310,150,fill="#bde3ff")
@@ -142,65 +152,109 @@ class PageOne(tk.Frame):
         CPU4.create_rectangle(40,150,175,250,fill="#bde3ff")
         CPU4.create_rectangle(175,150,310,250,fill="#bde3ff")
 
-        res = tk.Label(self)
-        res.place()
+        # Memory
+        MEMORY = tk.Canvas(self,height=470,width=450,bg="white")
+        MEMORY.place(x=570,y=520)
+        MEMORY.create_text(225,260,fill="Black", text=self.memString(), font=tkfont.Font(family='Helvetica', size=14, weight="bold") )
         entry = Entry(self)
-        entry.place()
-        test = tk.Button(self, text="Show text",
-                           command=lambda: self.updateData(entry,res))
-        test.place()
+        entry.place(x=160,y=33)
+
 
         button = tk.Button(self, text="Go to the start page",
                            command=lambda: controller.show_frame("StartPage"))
         button.place()
-        t1 = threading.Thread(target=processors[0].processorRoutine, args=[])
+        #t1 = threading.Thread(target=self.processors[0].processorRoutine, args=[])
+        #t1.start()
+        
+        self.updateData()
+        returnButton = tk.Button(self, text="Go to the start page",
+                           command=self.returnToStart)
+        returnButton.place(x=1350,y=30)
+        # MUST CREATE A START, PAUSE AND STOP BUTTON
+        startButton = tk.Button(self, text="Start", command=self.startThreads)
+        startButton.place(x=30,y=30)
+        stopButton = tk.Button(self, text="STOP", command=self.stopThreads)
+        stopButton.place(x=90,y=30)
+        # MUST ADD AN EXIT BUTTON
+        exitButton = tk.Button(self, text="Exit",
+                            command=self.exitFunction)
+        exitButton.place(x=1490,y=950)
+
+    def returnToStart(self):
+        # Stop the threads
+        for CPU in self.processors:
+            CPU.ACTIVE= False
+        # Clean the objects
+        # Return to Start Page
+        self.controller.show_frame("StartPage")
+    
+    def stopThreads(self):
+        # Stop the threads
+        for CPU in self.processors:
+            CPU.ACTIVE= False
+
+    def startThreads(self):
+        for CPU in self.processors:
+            CPU.ACTIVE= True
+        t1 = threading.Thread(target=self.processors[0].processorRoutine, args=[])
         t1.start()
-        self.updateData(processors,CACHE1_L1_1,CACHE1_L1_2,CACHE1_L1_3,CACHE1_L1_4)
-    def updateData(self,processors,c1,c2,c3,c4):
-        c1.delete(1.0,END)
-        c2.delete(1.0,END)
-        c3.delete(1.0,END)
-        c4.delete(1.0,END)
+        
+    def exitFunction(self):
+        tk.Tk.quit(self)
+
+    def updateData(self):
+        self.CACHE1_L1_1.delete(1.0,END)
+        self.CACHE1_L1_2.delete(1.0,END)
+        self.CACHE1_L1_3.delete(1.0,END)
+        self.CACHE1_L1_4.delete(1.0,END)
         #CACHE1_L1_1 = tk.Text(self, height = 4, width = 10, font=tkfont.Font(family='Helvetica', size=14, weight="bold"),bg="#bde3ff")
-        for key in processors[0].CACHE['B0']:
-            c1.insert(END, processors[0].CACHE['B0'][key]+"\n")
-        c1.place(x=92,y=140)
+        for key in self.processors[0].CACHE['B0']:
+            self.CACHE1_L1_1.insert(END, self.processors[0].CACHE['B0'][key]+"\n")
+        #self.CACHE1_L1_1.place(x=92,y=140)
 
         #CACHE1_L1_2 = tk.Text(self, height = 4, width = 10, font=tkfont.Font(family='Helvetica', size=14, weight="bold"),bg="#bde3ff")
-        for key in processors[0].CACHE['B1']:
-            c2.insert(END, processors[0].CACHE['B1'][key]+"\n")
-        c2.place(x=209,y=140)
+        for key in self.processors[0].CACHE['B1']:
+            self.CACHE1_L1_2.insert(END, self.processors[0].CACHE['B1'][key]+"\n")
+        #self.CACHE1_L1_2.place(x=209,y=140)
 
         #CACHE1_L1_3 = tk.Text(self, height = 4, width = 10, font=tkfont.Font(family='Helvetica', size=14, weight="bold"),bg="#bde3ff")
-        for key in processors[0].CACHE['B2']:
-            c3.insert(END, processors[0].CACHE['B2'][key]+"\n")
-        c3.place(x=92,y=235)
+        for key in self.processors[0].CACHE['B2']:
+            self.CACHE1_L1_3.insert(END, self.processors[0].CACHE['B2'][key]+"\n")
+        #self.CACHE1_L1_3.place(x=92,y=235)
 
         #CACHE1_L1_4 = tk.Text(self, height = 4, width = 10, font=tkfont.Font(family='Helvetica', size=14, weight="bold"),bg="#bde3ff")
-        for key in processors[0].CACHE['B3']:
-            c4.insert(END, processors[0].CACHE['B3'][key]+"\n")
-        c4.place(x=209,y=235)
-        self.update_idletasks()
-        
+        for key in self.processors[0].CACHE['B3']:
+            self.CACHE1_L1_4.insert(END, self.processors[0].CACHE['B3'][key]+"\n")
+        #self.CACHE1_L1_4.place(x=209,y=235)
+        self.after(100,self.updateData)
+    
+    def memString(self):
+        string = ""
+        for cell in self.memory.BLOCKS:
+            string = string+self.memory.BLOCKS[cell]+"\n\n\n"
+        print(string)
+        return string
 class PageTwo(tk.Frame):
 
-    def __init__(self, parent, controller,processors):
+    def __init__(self, parent, controller,processors,memory):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         label = tk.Label(self, text="continuous execution", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
         
+        
         button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
+                           command=self.returnToStart)
         button.pack()
-
+    def returnToStart(self):
+        # Stop the threads
+        # Clean the objects
+        # Return to Start Page
+        self.controller.show_frame("StartPage")
 
 if __name__ == "__main__":
     CPUS = []
     MEMORY = Memory()
     BUS = Bus(MEMORY)
     CPUS.append(Processor(1,BUS))
-    app = SampleApp(CPUS)
-    #app.geometry("1550x950")
-    #app.title("MOESI CACHE SIMULATOR")
-    #app.mainloop()
+    app = SampleApp(CPUS,MEMORY)
